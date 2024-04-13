06d1dcaf07def8adaba9f3787f7b2ef49cf51f4b
Stiven <codestiven@gmail.com>
2024-04-13 15:52:16 -0400

    modificacion flask

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '¡Hola, mundo!'
+
+@app.route('/saludo/<nombre>')
+def saludo(nombre):
+    return f'¡Hola, {nombre}!'
+
+@app.route('/template')
+def template():
+    return render_template('template.html', message='¡Hola desde la plantilla!')
 
 if __name__ == '__main__':
     app.run(debug=True)
