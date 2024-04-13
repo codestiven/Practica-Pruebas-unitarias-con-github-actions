0e84813f51056907f80440447a420175d4b4bdbc
Stiven <codestiven@gmail.com>
2024-04-13 19:18:35 -0400

    fewgw

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def saludo(nombre):
 
 @app.route('/template')
 def template():
-    return render_template('template.html', message='¡Hola desde la plantilla!')
+    return render_template('template.html', message='¡Hola desde la plantdilla!')
 
 if __name__ == '__main__':
     app.run(debug=True)
